a2081718c6cbdfa0cd58a4329398d391cf47a601
godlyggyt <118644958+godlyggyt@users.noreply.github.com>
2022-12-21 15:12:19 -0500

    Create cleanup.py

---
+++ cleanup.py
@@ -0,0 +1,14 @@
+import os
+
+def CleanupTempFiles():
+    # Code to delete temporary files goes here
+
+def CleanupRegistry():
+    # Code to remove unwanted registry entries goes here
+
+def DoCleanup():
+    CleanupTempFiles()
+    CleanupRegistry()
+
+if __name__ == "__main__":
+    DoCleanup()
